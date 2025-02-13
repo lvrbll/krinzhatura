0c5b32777115265dc4a99b1cbdc7cf0ac5ea9d89
Artem Harashchenko <9601339u@qed.local>
2025-02-13 16:49:06 +0100

    Initial commit

---
+++ app/src/main/java/com/example/krinzhatura/MainActivity.java
@@ -0,0 +1,155 @@
+package com.example.krinzhatura;
+
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+import android.widget.Toast;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
+
+
+public class MainActivity extends AppCompatActivity {
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
+    private Button button5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button1 = findViewById(R.id.button11);
+        button2 = findViewById(R.id.button22);
+        button3 = findViewById(R.id.button33);
+        button4 = findViewById(R.id.button44);
+        button5 = findViewById(R.id.button55);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showAlertDialogue();
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showListDialog();
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDataPickerDialog();
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showTimePickerDialog();
+            }
+        });
+
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showCustomDialog();
+            }
+        });
+    }
+
+    private void showAlertDialogue(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("KomediKlab i garik harlamov");
+        builder.setMessage("Sluga naroda i liga legend");
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this,
+                        "Kliknieto przycisk", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.setNegativeButton("Alo net", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this,
+                        "Kliknieto anuluj", Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.create().show();
+    }
+
+    private void showListDialog(){
+        final String[] items = {"Opcja1", "Opcja2", "Opcja3", "Opcja4", "Opcja5","Opcja6"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Wybierz opcję");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this, "Wybrano" + items[i],
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.create().show();
+    }
+
+    private void showDataPickerDialog(){
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+            new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                    Toast.makeText(MainActivity.this, "Wybrana data: " + day+"/"+(month+1)+"/"+year,
+                            Toast.LENGTH_SHORT).show();
+                }
+            }, year, month, day);
+        datePickerDialog.show();
+    }
+
+    private void showTimePickerDialog(){
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int hour, int minute) {
+                Toast.makeText(MainActivity.this, "Wybrany czas: " + hour+":"+minute,
+                        Toast.LENGTH_SHORT).show();
+            }
+        }, hour, minute, true);
+        timePickerDialog.show();
+    }
+
+    private void showCustomDialog(){
+        final android.app.Dialog dialog = new android.app.Dialog(this);
+        dialog.setContentView(R.layout.custom_dialog);
+
+        Button btnSubmit = dialog.findViewById(R.id.buttonSubmit);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Wysłano!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        dialog.show();
+
+    }
+}
